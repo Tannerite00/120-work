7c500088e6ec728227a34575ed40482f00d3374a
Tanner Dodd <31601567+Tannerite00@users.noreply.github.com>
2017-09-26 16:23:21 -0600

    Add files via upload

---
+++ 120-work/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // creating a canvas that is 600 pixels high by 600 pixels wide.
+createCanvas( 600, 600 )
+//color the background blue
+background('pink')
+}
+//decare the draw funcion
+function draw() {
+  //print line of text saying hello world
+  text( 'Hello World!', 30, 30)
+}
